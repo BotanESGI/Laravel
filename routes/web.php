56786d96528ec8a5b1dd829316a503e67625413c
Botan YILMAZ <byilmaz1@myges.fr>
2024-07-05 14:54:57 +0200

    Create AdminMiddleware

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\CartController;
+use App\Http\Middleware\AdminMiddleware;
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,11 +18,14 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/cart', [CartController::class, 'getCart'])->name('cart.get');
     Route::post('/paid', [CartController::class, 'paidCart'])->name('cart.paid');
     Route::delete('/cart/{id}', [CartController::class, 'removeProductCart'])->name('cart.product.delete');
     Route::put('/cart/{cart}', [CartController::class, 'updateProductQuantity'])->name('cart.product.update');
-
 });
+
+Route::middleware(['role.admin'])->group(function () {
+    Route::get('/cart', [CartController::class, 'getCart'])->name('cart.get');
+});
+
 
 require __DIR__.'/auth.php';
