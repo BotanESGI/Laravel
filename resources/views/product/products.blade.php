6ad8a17f44ad45fd2091fe60bf18879980bf117f
Botan YILMAZ <byilmaz1@myges.fr>
2024-07-05 19:06:41 +0200

    Finish Navbar

--- resources/views/product/products.blade.php
+++ resources/views/product/products.blade.php
@@ -3,6 +3,8 @@
 @section("content")
 <div class="py-12">
     <ul class="grid grid-cols-3 gap-5">
+        <a href="{{ route('product.create') }}" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Créer un produit</a>
+        {{ __('Ajouter des articles') }}
     @foreach($products as $product)
         <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <a href="#">
